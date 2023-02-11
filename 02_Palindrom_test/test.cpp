7c3879a4873f4dd9615462c918cf406055ab8bac
dennn93 <dennn93@mail.ru>
2023-02-11 20:02:54 +0300

    add simple tests

--- 02_Palindrom_test/test.cpp
+++ 02_Palindrom_test/test.cpp
@@ -1,6 +1,19 @@
 #include "pch.h"
+#include "../02_Palindrom/Palindrom.h"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+TEST(Palindrom, PalindromTrue) {
+
+	Solution s;
+	
+	EXPECT_EQ(s.isPalindrome(111), true);
+	EXPECT_EQ(s.isPalindrome(1221), true);
+	EXPECT_EQ(s.isPalindrome(12321), true);
+
+}
+
+
+int main(int argc, char** argv)
+{
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
 }
