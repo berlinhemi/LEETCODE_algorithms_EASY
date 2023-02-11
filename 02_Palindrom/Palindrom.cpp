f97e04812053c99fb00a14993a50075f9b2da1f0
dennn93 <dennn93@mail.ru>
2023-02-11 19:52:27 +0300

    some refactoring of Palindrome

--- 02_Palindrom/Palindrom.cpp
+++ 02_Palindrom/Palindrom.cpp
@@ -1,61 +1,9 @@
-﻿// Palindrom.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿
+//Given an integer x, return true if x is a palindrome, and false otherwise.
 
 #include <iostream>
 
-//v1 - tostring
-//#include <algorithm>
-//#include <string>
-//class Solution {
-//public:
-//    bool isPalindrome(int x) {
-//        std::string s = std::to_string(x);
-//        return(std::equal(s.begin(), s.begin() + s.size() / 2, s.rbegin()));
-//    }
-//};
 
-
-//v2 - array of digits
-//#include <algorithm>
-//#include <string>
-//class Solution {
-//public:
-//    unsigned GetNumberOfDigits(unsigned i)
-//    {
-//        return i > 0 ? (int)log10((double)i) + 1 : 1;
-//    }
-//
-//    bool isArrPalindrome(int* arr, int len)
-//    {
-//        int i = 0;
-//        while (i < len/2)
-//        {
-//            if (arr[i] != arr[len - i - 1])
-//                return false;
-//            i++;
-//        }
-//        return true;
-//    }
-//
-//    bool isPalindrome(int x) {
-//        if (x < 0)
-//            return false;
-//        int *number = new int[GetNumberOfDigits(x)]{};
-//        int i = 0;
-//        while (x > 0)
-//        {
-//            int r = x - (x/10)*10;
-//            //std::cout << r;
-//            number[i] = r;
-//            x /= 10;
-//            i++;
-//        }
-//        return isArrPalindrome(number,i);
-//    }
-//};
-
-
-//v3 - 
 class Solution {
 public:
     unsigned get_num_of_digits(unsigned i)
