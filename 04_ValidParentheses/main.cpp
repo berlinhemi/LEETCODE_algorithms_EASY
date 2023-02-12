fba46124ab342b8a75b00bf0f44c1323aec8828d
dennn93 <dennn93@mail.ru>
2023-02-12 22:15:12 +0300

    add new tests

--- 04_ValidParentheses/main.cpp
+++ 04_ValidParentheses/main.cpp
@@ -5,6 +5,16 @@ int main()
 {
     Solution s;
     std::cout << s.isValid("()[]{}") << std::endl;
+    std::cout << s.isValid("([]){([])}") << std::endl;
+    std::cout << s.isValid("(([]){})") << std::endl;
+
+    std::cout << s.isValid("[([]])") << std::endl;//should be false
+    std::cout << s.isValid("[({])}") << std::endl;//should be false
+
+
+    std::cout << s.isValid("([)]") << std::endl;
+    std::cout << s.isValid("([") << std::endl;
+    std::cout << s.isValid("([)]{}") << std::endl;
     std::cout << s.isValid("((([]){)})") << std::endl;
     std::cout << s.isValid("))(([]{}") << std::endl;
     std::cout << s.isValid("()[]{}}}{{") << std::endl;
