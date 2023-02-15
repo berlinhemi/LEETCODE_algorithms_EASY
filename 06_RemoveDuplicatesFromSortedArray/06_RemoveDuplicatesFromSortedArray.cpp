337bf716b515040d776bcd88f763abf2e95a6c01
dennn93 <dennn93@mail.ru>
2023-02-15 22:16:04 +0300

    rewrite solving of RemoveDuplicatesFromSortedArray: beats 68.99% runtime

--- 06_RemoveDuplicatesFromSortedArray/06_RemoveDuplicatesFromSortedArray.cpp
+++ 06_RemoveDuplicatesFromSortedArray/06_RemoveDuplicatesFromSortedArray.cpp
@@ -18,19 +18,19 @@ int Solution::removeDuplicates(std::vector<int>& nums)
 	int i = 1;
 	while (i < nums.size())
 	{
-		while (nums[i - 1] == nums[i] )
-		{	
+		if (nums[i - 1] == nums[i])
+		{
 			i++;
 			if (i == nums.size())
 				return insert_pos;
 		}
-		nums[insert_pos] = nums[i];
-		insert_pos++;
-		i++;
+		else
+		{
+			nums[insert_pos] = nums[i];
+			insert_pos++;
+			i++;
+		}
+		
 	}
 	return insert_pos;
-
-
-		
-	
 }
