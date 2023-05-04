fc231d94f9db22994f744b461133858d2e7efa4e
dennn93 <dennn93@mail.ru>
2023-05-04 16:12:11 +0300

    add task 11_AddBinary

---
+++ 11_AddBinary/11_AddBinary.cpp
@@ -0,0 +1,11 @@
+﻿#include "11_AddBinary.h"
+
+char Solution::AddDigit(char a, char b, char& rem);
+{
+
+}
+
+std::string Solution::addBinary(std::string a, std::string b)
+{
+
+}
---
+++ 11_AddBinary/11_AddBinary.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <string>
+class Solution {
+public:
+    std::string addBinary(std::string a, std::string b);
+};
