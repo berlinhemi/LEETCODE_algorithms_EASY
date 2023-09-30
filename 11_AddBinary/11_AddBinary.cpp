7703f76a99568d9b624ca376169c1858d2ede9b3
dennn93 <dennn93@mail.ru>
2023-09-30 18:04:55 +0300

    add main loop

--- 11_AddBinary/11_AddBinary.cpp
+++ 11_AddBinary/11_AddBinary.cpp
@@ -7,5 +7,16 @@ char Solution::AddDigit(char a, char b, char& rem);
 
 std::string Solution::addBinary(std::string a, std::string b)
 {
+	std::string result;
+	int max_len = a.length() > b.length() ? a.length() : b.length();
 
+	
+	for (int i = a.length() - 1, j = b.length() - 1, int k = 0;
+		i > 0 && j > 0;
+		i++, j++, k++)
+	{
+		char rem = '0';
+		char d = AddDigit(a[i], b[j], rem);
+		result = d + result;
+	}
 }
--- 11_AddBinary/11_AddBinary.h
+++ 11_AddBinary/11_AddBinary.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <string>
 class Solution {
+private:
+    char AddDigit(char a, char b, char& rem);
 public:
     std::string addBinary(std::string a, std::string b);
 };
