763a45aa7899deea1306c4907300fd1347e6e796
berlinhemi <dennn93@mail.ru>
2023-10-10 14:24:35 +0300

    add ClimbingStairs solution
    recursive solution gives TLE error

---
+++ 13_ClimbingStairs/13_ClimbingStairs.cpp
@@ -0,0 +1,16 @@
+﻿// 13_ClimbingStairs.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include "13_ClimbingStairs.h"
+
+
+int Solution::climbStairs(int n)
+{
+	if (n < 0)
+		return 0;
+	if (n == 0)
+		return 1;
+	return climbStairs(n - 1) + climbStairs(n - 2);
+}
+
---
+++ 13_ClimbingStairs/main.cpp
@@ -0,0 +1,9 @@
+#include "13_ClimbingStairs.h"
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    Solution s;
+    std::cout << s.climbStairs(5);
+}
