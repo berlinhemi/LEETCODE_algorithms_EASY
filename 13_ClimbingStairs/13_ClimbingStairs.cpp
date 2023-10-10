d258ec27524d964c877e6b8db45838b802e6b3f1
dennn93 <dennn93@mail.ru>
2023-10-10 16:58:49 +0300

    add non-recursive solution
    integer overflow in factorial

--- 13_ClimbingStairs/13_ClimbingStairs.cpp
+++ 13_ClimbingStairs/13_ClimbingStairs.cpp
@@ -4,13 +4,39 @@
 #include <iostream>
 #include "13_ClimbingStairs.h"
 
-
-int Solution::climbStairs(int n)
+int Solution::fact(int n)
+{
+	int res = 1;
+	for (int i = 2; i <= n; i++)
+	{
+		res *= i;
+	}
+	return res;
+}
+int Solution::climbStairs_rec(int n)
 {
 	if (n < 0)
 		return 0;
 	if (n == 0)
 		return 1;
-	return climbStairs(n - 1) + climbStairs(n - 2);
+	return climbStairs_rec(n - 1) + climbStairs_rec(n - 2);
 }
 
+int Solution::climbStairs(int n)
+{
+	int count = 1; // with step=1
+	// 1 1 1 1 1 1
+	//  2  1 1 1 1 :add one 2
+	//  2   2  1 1 :add second 2
+	//  2   2   2  :ass third 3
+	// and so on...
+	for (int i = 1; i <= n / 2; i++)
+	{
+		//how many ways are there to place k twos in n places?
+		int numemator = fact(n - i);
+		int denumenator = fact((n - i) - i) * fact(i);
+		count += numemator / denumenator;
+			
+	}
+	return count;
+}
--- 13_ClimbingStairs/13_ClimbingStairs.h
+++ 13_ClimbingStairs/13_ClimbingStairs.h
@@ -1,5 +1,8 @@
 #pragma once
 class Solution {
+private:
+    int fact(int n);
+    int climbStairs_rec(int n);
 public:
     int climbStairs(int n);
 };
