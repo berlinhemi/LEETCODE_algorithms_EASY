8ec9fd554272b6517ccd260a8751b633ff073cca
dennn93 <dennn93@mail.ru>
2023-10-17 22:07:55 +0300

    add MergeSortedArray

---
+++ 15_MergeSortedArrays/15_MergeSortedArrays.cpp
@@ -0,0 +1,9 @@
+﻿#include "15_MergeSortedArrays.h"
+
+
+
+void Solution::merge(std::vector<int>& nums1, int m, std::vector<int>& nums2, int n)
+{
+
+
+}
---
+++ 15_MergeSortedArrays/15_MergeSortedArrays.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <vector>
+class Solution {
+public:
+    void merge(std::vector<int>& nums1, int m, std::vector<int>& nums2, int n);
+};
