503066a745298922d75c14d51cdd446020b68c0e
dennn93 <dennn93@mail.ru>
2023-10-22 14:51:18 +0300

    add task 15

--- 15_MergeSortedArrays/15_MergeSortedArrays.cpp
+++ 15_MergeSortedArrays/15_MergeSortedArrays.cpp
@@ -1,9 +1,41 @@
 ﻿#include "15_MergeSortedArrays.h"
 
+#include <queue>
 
 
 void Solution::merge(std::vector<int>& nums1, int m, std::vector<int>& nums2, int n)
 {
 
-
+	//for (int i = 0, j = 0; i < m + n && j < n;);
+	std::queue<int> q;
+	int i = 0;
+	int j = 0;
+	while (i < m + n )
+	{
+		if (!q.empty() && nums2[j] > q.front())
+		{
+			if (nums1[i] == 0)
+			{
+				nums1[i] = q.front();
+			}
+			else if (nums1[i] > q.front() )
+			{
+				q.push(nums1[i]);
+				nums1[i] = q.front();
+			}
+			q.pop();
+		}
+		else if (nums1[i] == 0)
+		{
+			nums1[i] = nums2[j];
+			j++;
+		}
+		else if (nums1[i] > nums2[j])
+		{
+			q.push(nums1[i]);
+			nums1[i] = nums2[j];
+			j++;
+		}
+		i++;
+	}
 }
--- 15_MergeSortedArrays/main.cpp
+++ 15_MergeSortedArrays/main.cpp
@@ -1,6 +1,11 @@
+#include "15_MergeSortedArrays.h"
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    Solution s;
+    std::vector<int> v1{ 3,4,4,5,6,7,0,0,0,0,0 };
+    std::vector<int> v2{ 1,2,3,7,10 };
+    s.merge(v1,6,v2,5);
+    std::cout << std::endl;
 }
