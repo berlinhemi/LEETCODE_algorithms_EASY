5f76e8b6b86bfa036b7b7afaebd1a18d27ce5e6a
dennn93 <dennn93@mail.ru>
2023-11-11 11:08:45 +0300

    misc

--- 15_MergeSortedArrays/15_MergeSortedArrays.cpp
+++ 15_MergeSortedArrays/15_MergeSortedArrays.cpp
@@ -10,32 +10,48 @@ void Solution::merge(std::vector<int>& nums1, int m, std::vector<int>& nums2, in
 	std::queue<int> q;
 	int i = 0;
 	int j = 0;
-	while (i < m + n )
+	while (i < m + n)
 	{
-		if (!q.empty() && nums2[j] > q.front())
+		if (!q.empty())
+		{
+			if (j < n)
+			{
+				if (nums2[j] > q.front())
+				{
+					if (nums1[i] == 0)
+					{
+						nums1[i] = q.front();
+					}
+					else if (nums1[i] > q.front())
+					{
+						q.push(nums1[i]);
+						nums1[i] = q.front();
+					}
+					q.pop();
+				}
+			}
+			else
+			{
+				
+			}
+		}
+		else if (j < n)
 		{
 			if (nums1[i] == 0)
 			{
-				nums1[i] = q.front();
+				nums1[i] = nums2[j];
+				j++;
 			}
-			else if (nums1[i] > q.front() )
+			else if (nums1[i] > nums2[j])
 			{
 				q.push(nums1[i]);
-				nums1[i] = q.front();
+				nums1[i] = nums2[j];		
+				j++;
 			}
-			q.pop();
+			
 		}
-		else if (nums1[i] == 0)
-		{
-			nums1[i] = nums2[j];
-			j++;
-		}
-		else if (nums1[i] > nums2[j])
-		{
-			q.push(nums1[i]);
-			nums1[i] = nums2[j];
-			j++;
-		}
+		
+		
 		i++;
 	}
 }
--- 15_MergeSortedArrays/main.cpp
+++ 15_MergeSortedArrays/main.cpp
@@ -4,8 +4,27 @@
 int main()
 {
     Solution s;
-    std::vector<int> v1{ 3,4,4,5,6,7,0,0,0,0,0 };
-    std::vector<int> v2{ 1,2,3,7,10 };
-    s.merge(v1,6,v2,5);
-    std::cout << std::endl;
+
+    //std::vector<int> v1{2,0};
+    //std::vector<int> v2{ 1 };
+
+   // std::vector<int> v1{ 3,4,4,5,6,7,0,0,0,0,0 };
+    //std::vector<int> v2{ 1,2,3,7,10 };
+
+    //std::vector<int> v1{ 1,2,3,0,0,0 };
+    //std::vector<int> v2{ 2,5,6 };
+
+    std::vector<int> v1{ 4,5,6,0,0,0 };
+    std::vector<int> v2{ 1,2,3 };
+
+    //std::vector<int> v1{ 1,2,3,0,0,0 };
+    //std::vector<int> v2{ 4,5,6 };
+
+    s.merge(v1, 3, v2, 3);
+    //s.merge(v3,6,v4,3);
+    for (int e : v1)
+    {
+        std::cout << e <<" "<< std::endl;
+    }
+  
 }
