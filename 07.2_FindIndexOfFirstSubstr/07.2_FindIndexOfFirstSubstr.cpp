0c72947d5c0f7412e146cd45440f357ed2252805
berlinhemi <dennn93@mail.ru>
2025-09-06 18:05:30 +0300

    add formelly skipped problem

---
+++ 07.2_FindIndexOfFirstSubstr/07.2_FindIndexOfFirstSubstr.cpp
@@ -0,0 +1,12 @@
+﻿#include <string>
+
+
+class Solution {
+public:
+    int strStr(std::string haystack, std::string needle) {
+        size_t pos = haystack.find(needle);
+        if (pos != std::string::npos)
+            return pos;
+        else return -1;
+    }
+};
