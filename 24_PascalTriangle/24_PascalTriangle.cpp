fdf516cbb09cfa6fb9988d0454d7594a51808abf
berlinhemi <dennn93@mail.ru>
2025-09-28 21:32:16 +0300

    fix: remove extra actions

--- 24_PascalTriangle/24_PascalTriangle.cpp
+++ 24_PascalTriangle/24_PascalTriangle.cpp
@@ -10,20 +10,18 @@ public:
         result.push_back({1});
         if (numRows == 1)
             return result;
-        result.push_back({ 1,1 });
-        if (numRows == 2)
-            return result;
-        std::vector<int> curr{ 1,2,1 };
+
+        std::vector<int> curr{ 1,1 };
         result.push_back(curr);
-        for (int i = 4; i <= numRows; i++)
+
+        for (int i = 3; i <= numRows; i++)
         {
-            std::vector<int> next(curr.size() + 1);
-            next[0] = 1;
+            // fill next row with '1'
+            std::vector<int> next(curr.size() + 1, 1);
             for (int j = 1; j < curr.size(); j++)
             {
                 next[j] = curr[j-1] + curr[j];
             }
-            next[curr.size()] = 1;
             result.push_back(next);
             curr = next;
         }
