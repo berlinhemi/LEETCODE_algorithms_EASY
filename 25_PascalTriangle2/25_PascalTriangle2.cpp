9eb1852553367a42379a8a24351e99169fff6b32
berlinhemi <dennn93@mail.ru>
2025-10-06 21:07:15 +0300

    feat: add project for SellAndBuyStock task

--- 25_PascalTriangle2/25_PascalTriangle2.cpp
+++ 25_PascalTriangle2/25_PascalTriangle2.cpp
@@ -24,7 +24,6 @@ public:
         return curr;
 
     }
-    
 };
 
 int main()
---
+++ 26_BuyAndSellStock/26_BuyAndSellStock.cpp
@@ -0,0 +1,29 @@
+
+#include <iostream>
+#include <vector>
+
+
+class Solution {
+public:
+    int maxProfit(std::vector<int>& prices)
+    {
+
+    }
+};
+
+
+int main()
+{
+    Solution s;
+    std::vector<int> prices{};
+    int prices = s.maxProfit(prices);
+    //for (int e : result)
+    //{
+    //    //for (int e : v)
+    //    {
+    //        std::cout << e << " ";
+    //    }
+    //    //std::cout << std::endl;
+    //}
+    std::cout << std::endl;
+}
