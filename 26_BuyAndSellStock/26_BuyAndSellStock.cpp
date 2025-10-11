2e4d4fdc8bf2038dab42132db497441a0c0a62f0
berlinhemi <dennn93@mail.ru>
2025-10-11 16:27:00 +0300

    feat: add possible solution of task 26

--- 26_BuyAndSellStock/26_BuyAndSellStock.cpp
+++ 26_BuyAndSellStock/26_BuyAndSellStock.cpp
@@ -1,22 +1,63 @@
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 
 class Solution {
 public:
+
+   
     int maxProfit(std::vector<int>& prices)
     {
+        std::vector<int> profits = getProfits(prices);
+        auto maxProfit = std::max_element(profits.begin(), profits.end());
 
+        
+        return (maxProfit == profits.end()) ? 0 : *maxProfit;
+    }
+
+private:
+    std::vector<int> getProfits(std::vector<int>& prices)
+    {
+        std::vector<int> profits;
+
+        int sell_price = 0;
+        int buy_price = prices[0];
+
+        for (int i = 1; i < prices.size(); i++)
+        {
+            // Detect price increasing
+            if (prices[i - 1] <= prices[i])
+            {
+                if (prices[i] > sell_price)
+                    if (prices[i] > buy_price)
+                        profits.push_back(prices[i] - buy_price);
+                sell_price = prices[i];
+            }
+            // Price started to fall
+            else
+            {
+                // save profit 
+                if (sell_price > buy_price)
+                    profits.push_back(sell_price - buy_price);
+                // update best price
+                if (buy_price > prices[i])
+                {
+                    buy_price = prices[i];
+                }
+            }
+        }
+        return profits;
     }
 };
-
 
 int main()
 {
     Solution s;
-    std::vector<int> prices{};
-    int prices = s.maxProfit(prices);
+     std::vector<int> prices{ 7,1,5,3,6,4 };
+    //std::vector<int> prices{10, 9, 4, 2, 1};
+    int profit = s.maxProfit(prices);
     //for (int e : result)
     //{
     //    //for (int e : v)
@@ -25,5 +66,5 @@ int main()
     //    }
     //    //std::cout << std::endl;
     //}
-    std::cout << std::endl;
+    std::cout << profit<< std::endl;
 }
