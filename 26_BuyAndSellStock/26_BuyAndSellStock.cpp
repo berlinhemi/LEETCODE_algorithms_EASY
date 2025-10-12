b7613c1635b36117222fa4035622186aaa9de419
berlinhemi <dennn93@mail.ru>
2025-10-12 13:24:14 +0300

    refactor: add more easy and readable solution for task 26

--- 26_BuyAndSellStock/26_BuyAndSellStock.cpp
+++ 26_BuyAndSellStock/26_BuyAndSellStock.cpp
@@ -10,69 +10,36 @@ public:
    
     int maxProfit(std::vector<int>& prices)
     {
-        std::vector<int> profits = getProfits(prices);
-
-        for (int e : profits)
-        {
-            std::cout << e << " ";
-
-        }
-        std::cout << std::endl;
-        auto maxProfit = std::max_element(profits.begin(), profits.end());
-
-        
-        return (maxProfit == profits.end()) ? 0 : *maxProfit;
-    }
-
-private:
-    std::vector<int> getProfits(std::vector<int>& prices)
-    {
-        std::vector<int> profits;
-
+        int profit = 0;
         int sell_price = 0;
         int buy_price = prices[0];
 
         for (int i = 1; i < prices.size(); i++)
         {
-            // Detect price increasing
-            if (prices[i - 1] <= prices[i])
-            {
-                // save profit if current price is good
-                if (prices[i] >= sell_price)
-                    if (prices[i] > buy_price) {
-                        std::cout << "add 1:" << prices[i] - buy_price << std::endl;
-                        profits.push_back(prices[i] - buy_price);
-                    }
-                sell_price = prices[i];
-            }
-            // Price started to fall
+            if (prices[i] < buy_price)
+                buy_price = prices[i];
             else
             {
-                // save profit 
-                if (sell_price > buy_price) {
-                    
-                    std::cout << "add 2:" << sell_price - buy_price << std::endl;
-                        profits.push_back(sell_price - buy_price);
-                }
-                // update best price
-                if (buy_price > prices[i])
-                {
-                    buy_price = prices[i];
-                    // reset sell price
-                    sell_price = 0;
-                }
+                // calculate current profit
+                if (prices[i] - buy_price > profit)
+                    profit = prices[i] - buy_price;
             }
         }
-        return profits;
+        return profit;
     }
 };
 
 int main()
 {
     Solution s;
-     std::vector<int> prices{ 4,7,2,1 };
-    //std::vector<int> prices{10, 9, 4, 2, 1};
-    int profit = s.maxProfit(prices);
+    std::vector<int> prices1{ 4,7,2,1 };
+    std::vector<int> prices2{1,2,4,2,5,7,2,4,9,0,9};
+    std::vector<int> prices3{10, 9, 4, 2, 1};
+    
+    std::cout << s.maxProfit(prices1) << std::endl;
+    std::cout << s.maxProfit(prices2) << std::endl;
+    std::cout << s.maxProfit(prices3) << std::endl;
+
     //for (int e : result)
     //{
     //    //for (int e : v)
@@ -81,5 +48,4 @@ int main()
     //    }
     //    //std::cout << std::endl;
     //}
-    std::cout << profit<< std::endl;
 }
